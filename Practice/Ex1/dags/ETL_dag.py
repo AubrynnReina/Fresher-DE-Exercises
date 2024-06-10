20a24065d2a82615266fb0c201e71ba1cc145a41
Anh Nguyễn Trí <78487345+ChariskYuri@users.noreply.github.com>
2024-06-10 15:08:56 +0700

    Theory 3 + XCom deletion

--- Practice/Ex1/dags/ETL_dag.py
+++ Practice/Ex1/dags/ETL_dag.py
@@ -4,12 +4,19 @@ from airflow.operators.python_operator import PythonOperator
 from extract import extract
 from transform import transform
 from load import load
+from airflow.utils.db import provide_session
+from airflow.models import XCom
 
+@provide_session
+def clean_xcom(session=None, **context):
+    dag = context["dag"]
+    dag_id = dag._dag_id
+    session.query(XCom).filter(XCom.dag_id == dag_id).delete()
 
 dag = DAG(
     dag_id='ETL_dag',
     default_args={
-        'start_date': days_ago(1),
+        'start_date': days_ago(0),
     },
     schedule_interval='*/2 * * * *',
     catchup=False
@@ -33,5 +40,17 @@ load = PythonOperator(
     dag=dag
 )
 
-extract >> transform >> load
+# cleanup_xcom = PythonOperator(
+#     task_id='cleanup_xcom',
+#     python_callable=cleanup_xcom,
+#     dag=dag
+# )
+
+delete_xcom = PythonOperator(
+    task_id="delete_xcom",
+    python_callable = clean_xcom, 
+    dag=dag
+)
+
+extract >> transform >> load >> delete_xcom
 # transform >> [load, load_2] # parallel
