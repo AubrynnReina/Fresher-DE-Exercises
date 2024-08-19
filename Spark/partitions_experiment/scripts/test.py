5b10068ae7b365e5ddc479113ed3f23b4b99bd1c
Anh Nguyễn Trí <78487345+ChariskYuri@users.noreply.github.com>
2024-08-19 16:01:55 +0700

    complete experiment script and generate more data

--- Spark/partitions_experiment/scripts/test.py
+++ Spark/partitions_experiment/scripts/test.py
@@ -1,7 +1,16 @@
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import col, udf
+from pyspark.sql.types import BooleanType
+
+import time
+import pickle
+
+@udf(returnType=BooleanType())
 def prime_number_detector(num):
 
     import math
 
+    num = int(num)
     if num in [0, 1]:
         return False
 
@@ -13,9 +22,19 @@ def prime_number_detector(num):
     return True
 
 
-from pyspark.sql import SparkSession
+time_data = []
 
-spark_session = SparkSession.builder.appName("Test").getOrCreate()
+for i in range(6, 91, 6):
+    spark_session = SparkSession.builder.appName("Test").getOrCreate()
 
-data_df = spark_session.read.text('../data/prime.txt')
-print(data_df.count())
+    data_df = spark_session.read.text('../data/prime.txt')
+    prime_nums = data_df[prime_number_detector(data_df['value'])]
+
+    start_time = time.time()
+    print(prime_nums.count())
+    end_time = time.time()
+
+    time_data.append(end_time - start_time)
+
+with open('../data/Exe_time_2_3_2.pkl', 'wb') as f:
+    pickle.dump(time_data, f)
